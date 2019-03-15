28417cd4aad0046d8e19c84ac48e2e47bdea65fd
Reynier Blanco Zambrano <rblancoz85@gmail.com>
2019-03-15 14:54:47 -0300

    ready

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+// import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  // constructor(private iab: InAppBrowser) { 
+  //   const browser = this.iab.create('https://www.google.cl/');
+
+  //   browser.on('loadstop').subscribe(event => {
+  //     browser.insertCSS({ code: "body{color: red;" });
+  //   });
+
+  //   browser.close();
+  // }
+}
